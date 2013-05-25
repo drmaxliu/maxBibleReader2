7e7c7cb840e39bc7baf0a0cbdc2b9b8cb73c3e9f
Max Liu <dr.max.liu@gmail.com>
2013-05-25 07:21:22 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaxBibleReader2::Application.config.secret_token = '7e29e4d755b8cd4de8c25d4c663034863613295ea8a3ec10f704ba6ad79b093655cbe85d589c5991b64d941333b23e9ad42f2f676946dab8d1c2291f6074ae3c'
